c96e291405eb0488f64c7c5c48b6351cde7ce320
8bitzz <hanu.nguyenhang@gmail.com>
2019-05-07 21:36:18 +0700

    initial commit

---
+++ M4-challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  M4-challenge
+//
+//  Created by Hang Nguyen on 5/7/19.
+//  Copyright © 2019 8bitzz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
