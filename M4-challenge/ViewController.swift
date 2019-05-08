d25c70767ce1027ef401fca0a7ecca3652245dca
8bitzz <hanu.nguyenhang@gmail.com>
2019-05-08 20:33:48 +0700

    add-podfile

--- M4-challenge/ViewController.swift
+++ M4-challenge/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import UnsplashPhotoPicker
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        UnsplashPhotoPickerConfiguration(accessKey: <#T##String#>, secretKey: )
+        
     }
 
 
---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '11.0'
+use_frameworks!
+
+target 'M4-challenge' do
+    pod 'UnsplashPhotoPicker', '~> 1.0'
+    
+end
