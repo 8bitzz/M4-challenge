edcd0da83d6863603108b73e8c1de8f71af9549f
8bitzz <hanu.nguyenhang@gmail.com>
2019-05-09 22:09:08 +0700

    basic ui

---
+++ M4-challenge/PhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCell.swift
+//  M4-challenge
+//
+//  Created by Hang Nguyen on 5/9/19.
+//  Copyright © 2019 8bitzz. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageName: UILabel!
+    
+}
