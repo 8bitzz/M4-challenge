d8b2fd8f46a643b097a163bc7b4e8b63b21f75fd
8bitzz <hanu.nguyenhang@gmail.com>
2019-05-18 22:05:00 +0700

    prepare layout for editing photo

---
+++ M4-challenge/DetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailViewController.swift
+//  M4-challenge
+//
+//  Created by Hang Nguyen on 5/18/19.
+//  Copyright © 2019 8bitzz. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UILabel!
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
